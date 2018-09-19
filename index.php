0fea2391b2dc145e48446fe0e488a244f53b93a7
Douglas de Alvarenga <dalvarenga.857@gmail.com>
2018-09-19 19:08:41 -0300

    metedos de lista

--- index.php
+++ index.php
@@ -1,11 +1,23 @@
 <?php
 	require_once("config.php");
 
-	$root = new Usuario();
+	//Carrega um usuário
+	//$root = new Usuario();
+	//$root->loadById(18);
+	//echo $root;
 
-	$root->loadById(18);
+	//Carrega uma lista de usuários
+	//$lista = Usuario::getList();
+	//echo json_encode($lista);
 
-	echo $root;
+
+	//Carrega uma lista de usuários buscando pelo login
+	//$search = Usuario::search("ou");
+	//echo json_encode($search);
+
+	$usuario = new Usuario();
+	$usuario->login("root","!@#$%");
+	echo $usuario;
 ?>
 
 
